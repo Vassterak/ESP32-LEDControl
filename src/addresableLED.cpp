bc943fe64548373af313b9f8d2bdfef2933650da
Vassterak <vaclavhorcicka@outlook.cz>
2021-07-10 00:38:32 +0200

    rainbow animation, solid colors Addresable strip

--- src/addresableLED.cpp
+++ src/addresableLED.cpp
@@ -1,7 +1,25 @@
 #include "addresableLED.h"
 #include <FastLED.h>
 
-void AddresableLED::Duha(CRGB addLEDs[],uint8_t numberOfLEDs, uint8_t HUEIncrementor)
+void AddresableLED::StaticRainbow(CRGB LEDstrip[],uint8_t numberOfLEDs, uint8_t HUEIncrementor)
 {
-    fill_rainbow(addLEDs, numberOfLEDs, 0, HUEIncrementor);
+    fill_rainbow(LEDstrip, numberOfLEDs, 0, HUEIncrementor);
+}
+void AddresableLED::AnimeRainbow(CRGB LEDstrip[],uint8_t numberOfLEDs,uint8_t speed, uint8_t HUEIncrementor) 
+{
+    uint8_t hue = beat8(speed, 255);
+    fill_rainbow(LEDstrip, numberOfLEDs, hue, HUEIncrementor);
+}
+
+void AddresableLED::SolidPart1(CRGB LEDstrip[], CRGB color)
+{
+    fill_solid(LEDstrip, 60, color);
+}
+void AddresableLED::SolidPart2(CRGB LEDstrip[], CRGB color)
+{
+    fill_solid(LEDstrip+60, 89, color);
+}
+void AddresableLED::SolidPart3(CRGB LEDstrip[], CRGB color)
+{
+    fill_solid(LEDstrip+89, 149, color);
 }
--- src/addresableLED.h
+++ src/addresableLED.h
@@ -2,6 +2,12 @@
 class AddresableLED
 {
     public:
-    void Duha(CRGB addLEDs[],uint8_t numberOfLEDs, uint8_t HUEIncrementor);
-    
+    void StaticRainbow(CRGB LEDstrip[],uint8_t numberOfLEDs, uint8_t HUEIncrementor);
+    void AnimeRainbow(CRGB LEDstrip[],uint8_t numberOfLEDs,uint8_t speed, uint8_t HUEIncrementor); 
+
+    void SolidPart1(CRGB LEDstrip[], CRGB color); //right side of bed
+    void SolidPart2(CRGB LEDstrip[], CRGB color); //center
+    void SolidPart3(CRGB LEDstrip[], CRGB color); //left side of bed
+
+
 };
