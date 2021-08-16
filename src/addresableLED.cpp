ad8ae4625666cf94a4664eceb6347cabf62d058d
Vassterak <vaclavhorcicka@outlook.cz>
2021-08-16 23:49:05 +0200

    Move from SRGB to CHSV color profile

--- src/addresableLED.cpp
+++ src/addresableLED.cpp
@@ -4,7 +4,7 @@ enum{
 
 };
 
-void AddresableLED::SolidPart(CRGB color, int fromZone, int toZone)
+void AddresableLED::SolidPart(CHSV color, int fromZone, int toZone)
 {
     fill_solid(leds+fromZone, toZone, color);
 }
@@ -20,14 +20,14 @@ void AddresableLED::AnimeRainbow(uint8_t speed, uint8_t HUEIncrementor)
     fill_rainbow(leds, LEDS_TOTAL_NUMBER, hue, HUEIncrementor);
 }
 
-void AddresableLED::FallingStars(uint8_t speedOfDisappear, CRGB color) 
+void AddresableLED::FallingStars(uint8_t speedOfDisappear, CHSV color) 
 {
-    leds[random8(0, 149-1)] = CRGB(color);
+    leds[random8(0, 149-1)] = CHSV(color);
     //speedOfDisappear is how much brightness will decrese per frame 0-255
     fadeToBlackBy(leds, LEDS_TOTAL_NUMBER, speedOfDisappear);
 }
 
-void AddresableLED::Fade1(uint8_t speedOfTransition, CRGB color1, CRGB color2)
+void AddresableLED::Fade1(uint8_t speedOfTransition, CHSV color1, CHSV color2)
 {
     nblend(color1, color2, speedOfTransition);
 }
--- src/addresableLED.h
+++ src/addresableLED.h
@@ -10,16 +10,16 @@ class AddresableLED
         FastLED.addLeds<NEOPIXEL,LEDS_DATA_PIN>(leds, LEDS_TOTAL_NUMBER);
     }
 
-    void SolidPart(CRGB color, int fromZone, int toZone); //right side of bed
-    void SolidPart2(CRGB color); //center
-    void SolidPart3(CRGB color); //left side of bed
+    void SolidPart(CHSV color, int fromZone, int toZone); //right side of bed
+    void SolidPart2(CHSV color); //center
+    void SolidPart3(CHSV color); //left side of bed
 
     // whole strip
     void StaticRainbow(uint8_t HUEIncrementor);
     void AnimeRainbow(uint8_t speed, uint8_t HUEIncrementor);
-    void FallingStars(uint8_t speedOfDisappear, CRGB color);
-    void Fade1(uint8_t speedOfTransition, CRGB color1, CRGB color2);
-    void Fade2(uint8_t speedOfTransition, CRGB color1, CRGB color2, CRGB color3);
-    void Fade3(uint8_t speedOfTransition, CRGB color1, CRGB color2, CRGB color3, CRGB color4);
-    void MovingToEachOther(uint8_t speed, CRGB color1, CRGB color2);
+    void FallingStars(uint8_t speedOfDisappear, CHSV color);
+    void Fade1(uint8_t speedOfTransition, CHSV color1, CHSV color2);
+    void Fade2(uint8_t speedOfTransition, CHSV color1, CHSV color2, CHSV color3);
+    void Fade3(uint8_t speedOfTransition, CHSV color1, CHSV color2, CHSV color3, CHSV color4);
+    void MovingToEachOther(uint8_t speed, CHSV color1, CHSV color2);
 };
