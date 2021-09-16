185649905fb00d0c40dac86f43421998cdb88631
Vassterak <vaclavhorcicka@outlook.cz>
2021-09-17 00:44:24 +0200

    Adding definitions (global variables list)

---
+++ src/definitions.h
@@ -0,0 +1,45 @@
+#ifndef DEFINITIONS_H
+#define DEFINITIONS_H
+
+//Clasic LED strip settings-----------------------------------------------------
+#define LED_R 25 //pins
+#define LED_G 26
+#define LED_B 27
+
+const int PWMFreq = 100; // 100Hz frequency is double of speed of animation which is in 50FPS so the flickering is not visible. If your grid uses 60Hz and you see flickering. Change it to 120Hz and refresh rate to 60FPS
+const int PWMChannelR = 0, PWMChannelG = 1, PWMChannelB = 2; //Channel for each color
+const int PWMResolution = 8; // 8bits, from 0 (off state) to 255 (max brightness) 
+const int MAX_DUTY_CYCLE = (int)((PWMResolution * PWMResolution) -1 ); //calculates duty cicle from resolution
+
+//Addressable LED strip---------------------------------------------------------
+#define LEDS_DATA_PIN 22 //IO pin
+#define LEDS_TOTAL_NUMBER 149 //for addresable LED, 149 because one LED is dead on my strip with 150 leds
+#define LEDS_ZONE_PART1 60
+#define LEDS_ZONE_PART2 89
+//#define LEDS_ZONE_PART3 149 //Not required because 149 is total number of leds (so it's also the last one in the strip)
+
+//Timing------------------------------------------------------------
+// In the mobile app there is settings for speed (controls transition and speed of effects)
+// In the app you can change it from 20ms to 5000ms THIS IS WHAT APP IS SENDING! SO IF YOU WANT TO CHANGE THE RANGE, YOU NEED TO CHANGE IT HERE ALSO.
+// But when you look up the Rainbow() you will see that the speed is controlled by addition to hue that will overflow to 0 so it's repeating indefinitely.
+// So here you can set how agressive it will be. For more understanding open proccessingFuctions.cpp and look for EffectSpeedRemap()
+#define SPEED_VALUE_MIN 1
+#define SPEED_VALUE_MAX 40
+
+#define SPEED_TIME_MIN 20 //ms
+#define SPEED_TIME_MAX 5000 //ms
+
+//Bluetooth----------------------------------------------------------
+#define BUTTON_PIN 32 //IO14 Pin nuber 13
+#define BLE_DEV_NAME "LED Controller" //Visible bluetooth name of the ESP32
+#define BLE_PASSCODE 123456 //Set your own passcode for pairing!
+#define SERVICE_UUID "Your custom UUID! replace this string!"
+
+//CHARACTERISTIC_UUIDs each one has to be unique!
+#define LED_ZONE_SELECT     "Your custom UUID! replace this string!"    //zones for addresable led strip
+#define LED_MODE_SELECT     "Your custom UUID! replace this string!"    //modes to switch (addreable/classic strip/zone mode/whole strip)
+#define LED_EFFECT_SELECT   "Your custom UUID! replace this string!"    //effect code to apply
+#define LED_COLOR           "Your custom UUID! replace this string!"    //color for effect, including brightness
+#define LED_SPEED           "Your custom UUID! replace this string!"    //speed of the effect/additional variable for usage of complex effects
+
+#endif
