fcf7733e46840031dd5b264f0ad75c3cfee25c9b
Vassterak <vaclavhorcicka@outlook.cz>
2021-11-14 21:05:44 +0100

    Scheme change from argument to class's instance fields

--- src/addresableLED.cpp
+++ src/addresableLED.cpp
@@ -1,31 +1,31 @@
 #include "addresableLED.h"
 
-void AddresableLED::SolidPart(CHSV color, int fromZone, int toZone)
+void AddresableLED::SolidPart()
 {
-    fill_solid(leds+fromZone, toZone, color);
+    fill_solid(leds+0, LEDS_TOTAL_NUMBER, setColors[0]);
 }
 
-void AddresableLED::staticRainbow(uint8_t HUEIncrementor)
+void AddresableLED::staticRainbow()
 {
-    fill_rainbow(leds, LEDS_TOTAL_NUMBER, 0, HUEIncrementor);
+    fill_rainbow(leds, LEDS_TOTAL_NUMBER, 0, currentSpeed);
 }
 
-void AddresableLED::animeRainbow(uint8_t speed, uint8_t HUEIncrementor) 
+void AddresableLED::animeRainbow() 
 {
-    uint8_t hue = beat8(speed, 255);
-    fill_rainbow(leds, LEDS_TOTAL_NUMBER, hue, HUEIncrementor);
+    uint8_t hue = beat8(currentSpeed, 255);
+    fill_rainbow(leds, LEDS_TOTAL_NUMBER, hue, currentSpeed);
 }
 
-void AddresableLED::fallingStars(uint8_t speedOfDisappear, CHSV color) 
+void AddresableLED::fallingStars() 
 {
-    leds[random8(0, 149-1)] = CHSV(color);
+    leds[random8(0, 149-1)] = CHSV(setColors[0]);
     //speedOfDisappear is how much brightness will decrese per frame 0-255
-    fadeToBlackBy(leds, LEDS_TOTAL_NUMBER, speedOfDisappear);
+    fadeToBlackBy(leds, LEDS_TOTAL_NUMBER, currentSpeed);
 }
 
-void AddresableLED::fade1(uint8_t speedOfTransition, CHSV color1, CHSV color2)
+/* void AddresableLED::fade1(uint8_t speedOfTransition, CHSV color1, CHSV color2)
 {
     nblend(color1, color2, speedOfTransition);
-}
+} */
 
 
--- src/addresableLED.h
+++ src/addresableLED.h
@@ -13,14 +13,14 @@ class AddresableLED
         FastLED.addLeds<NEOPIXEL,LEDS_DATA_PIN>(leds, LEDS_TOTAL_NUMBER);
     }
 
-    void SolidPart(CHSV color, int fromZone, int toZone); //right side of bed
+    void SolidPart();
 
     // whole strip
-    void staticRainbow(uint8_t HUEIncrementor);
-    void animeRainbow(uint8_t speed, uint8_t HUEIncrementor);
-    void fallingStars(uint8_t speedOfDisappear, CHSV color);
-    void fade1(uint8_t speedOfTransition, CHSV color1, CHSV color2);
+    void staticRainbow();
+    void animeRainbow();
+    void fallingStars();
+/*     void fade1(uint8_t speedOfTransition, CHSV color1, CHSV color2);
     void fade2(uint8_t speedOfTransition, CHSV color1, CHSV color2, CHSV color3);
     void fade3(uint8_t speedOfTransition, CHSV color1, CHSV color2, CHSV color3, CHSV color4);
-    void movingToEachOther(uint8_t speed, CHSV color1, CHSV color2);
+    void movingToEachOther(uint8_t speed, CHSV color1, CHSV color2); */
 };
